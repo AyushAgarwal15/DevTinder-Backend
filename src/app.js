ee6b632ea717a1201208f4736e13a43303f33d2e
Ayush Agarwal <ayushagarwal8126@gmail.com>
2025-04-08 18:33:56 +0530

    Added Data Validation

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const connectDB = require("./config/database");
-const app = express();
 const User = require("./models/user");
+const app = express();
 
 app.use(express.json());
 
@@ -15,6 +15,85 @@ app.post("/signup", async (req, res) => {
     res.send("User created successfully");
   } catch (err) {
     res.status(400).send("Error creating the user " + err.message);
+  }
+});
+
+app.get("/feed", async (req, res) => {
+  try {
+    const users = await User.find();
+    if (users.length == 0) {
+      throw new Error("No users found");
+    } else {
+      res.send(users);
+    }
+  } catch (err) {
+    res.status(400).send("Something went wrong" + err.message);
+  }
+});
+
+app.get("/user", async (req, res) => {
+  try {
+    const userEmail = req.body.emailId;
+    const userId = req.body.id;
+
+    let user;
+
+    if (userEmail) {
+      user = await User.findOne({ emailId: userEmail });
+    } else if (userId) {
+      user = await User.findById(userId);
+    }
+
+    if (!user) {
+      throw new Error("No user found.");
+    } else {
+      res.send(user);
+    }
+  } catch (err) {
+    res.status(500).send("Something went wrong: " + err.message);
+  }
+});
+
+app.delete("/user", async (req, res) => {
+  try {
+    const userId = req.body.id;
+    await User.findByIdAndDelete(userId);
+    res.send("User deleted successfully");
+  } catch (err) {
+    res.status(400).send("Error deleting the user " + err.message);
+  }
+});
+
+app.patch("/user/:userId", async (req, res) => {
+  try {
+    const userId = req.params?.userId;
+    const data = req.body;
+
+    const ALLOWED_UPDATE_KEYS = [
+      "age",
+      "gender",
+      "location",
+      "photoUrl",
+      "about",
+      "skills",
+      "password",
+    ];
+
+    const isUpdateAllowed = Object.keys(data).every((key) =>
+      ALLOWED_UPDATE_KEYS.includes(key)
+    );
+
+    if (!isUpdateAllowed) {
+      throw new Error("Invalid update keys");
+    }
+
+    await User.findByIdAndUpdate(userId, data, {
+      returnDocument: "after",
+      runValidators: true,
+    });
+    res.send("User updated successfully");
+  } catch (err) {
+    res.status(400).send("Error updating user: " + err.message);
   }
 });
 
--- src/models/user.js
+++ src/models/user.js
@@ -1,27 +1,100 @@
 const mongoose = require("mongoose");
+const validator = require("validator");
 
-const userSchema = new mongoose.Schema({
-  firstName: {
-    type: String,
+const userSchema = new mongoose.Schema(
+  {
+    firstName: {
+      type: String,
+      required: true,
+      minlength: 3,
+      maxlength: 50,
+      trim: true,
+    },
+    lastName: {
+      type: String,
+      minlength: 3,
+      maxlength: 50,
+      trim: true,
+    },
+    emailId: {
+      type: String,
+      unique: true,
+      required: true,
+      lowercase: true,
+      trim: true,
+      // custom way of adding validations
+      // validate: {
+      //   validator: function (v) {
+      //     return /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/.test(v);
+      //   },
+      //   message: (props) => `${props.value} is not a valid email!`,
+      // },
+      // library way of adding validations
+      validate(value) {
+        if (!validator.isEmail(value)) {
+          throw new Error("Invalid Email: " + value);
+        }
+      },
+    },
+    password: {
+      type: String,
+      required: true,
+      minlength: [
+        6,
+        " Password must be at least 6 characters long, got {VALUE}",
+      ],
+      maxlength: 100,
+      validate(value) {
+        if (!validator.isStrongPassword(value)) {
+          throw new Error("Password is not strong enough");
+        }
+      },
+    },
+    age: {
+      type: Number,
+      min: 18,
+    },
+    gender: {
+      type: String,
+      enum: ["male", "female", "other"],
+      required: true,
+    },
+    location: {
+      type: String,
+      trim: true,
+      maxlength: 100,
+    },
+    photoUrl: {
+      type: String,
+      default:
+        "https://tamilnaducouncil.ac.in/wp-content/uploads/2020/04/dummy-avatar.jpg",
+      validate(value) {
+        if (!validator.isURL(value)) {
+          throw new Error("Invalid URL: " + value);
+        }
+      },
+    },
+    about: {
+      type: String,
+      default: "Open to make new connections 🙂",
+      trim: true,
+      maxlength: 200,
+    },
+    skills: {
+      type: [String],
+      validate: [
+        {
+          validator: (arr) => arr.length <= 5,
+          message: "You can only add up to 5 skills.",
+        },
+        {
+          validator: (arr) => arr.every((skill) => skill.length <= 20),
+          message: "Each skill must be at most 20 characters long.",
+        },
+      ],
+    },
   },
-  lastName: {
-    type: String,
-  },
-  emailId: {
-    type: String,
-  },
-  password: {
-    type: String,
-  },
-  gender: {
-    type: String,
-  },
-  age: {
-    type: Number,
-  },
-  location: {
-    type: String,
-  },
-});
+  { timestamps: true }
+);
 
 module.exports = mongoose.model("User", userSchema);
