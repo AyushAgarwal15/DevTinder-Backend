9b0b39a4077fda9e537329271c95199f5b6eab8b
Ayush Agarwal <ayushagarwal8126@gmail.com>
2025-05-27 21:35:03 +0530

    fixed mongo issue

--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -6,7 +6,7 @@ const userAuth = async (req, res, next) => {
     // Read the token from the req cookies
     const token = req.cookies.token;
     if (!token) {
-      return res.status(401).send("Please login!");
+      return res.status(401).send("Please login! ");
     }
 
     // Validate the token
